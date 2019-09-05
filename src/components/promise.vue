575599ea2e409fde9f62a1984d4a3d448425bfbe
xiaosongread <598349146@qq.com>
2019-09-05 15:14:43 +0800

    fix

---
+++ src/components/promise.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+111
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+  computed: {
+
+  },
+  created() {
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ test: 1 })
+            resolve({ test: 2 })
+            // reject({ test: 3 })
+        }, 1000)
+    }).then((data) => {
+        console.log('result1', data)
+    },(data1)=>{
+        console.log('result2',data1)
+    }).then((data) => {
+        console.log('result3', data)
+    })
+  },
+  methods: {
+
+  },
+  components: {
+
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Key from '@/components/key'
+import Promise1 from '@/components/promise'
 
 
 Vue.use(Router)
@@ -17,6 +18,11 @@ export default new Router({
       path: '/key',
       name: 'Key',
       component: Key
+    },
+    {
+      path: '/promise1',
+      name: 'Promise1',
+      component: Promise1
     }
   ]
 })
